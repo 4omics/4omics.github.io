24a8862fd33af8e2eda8be4d398a8925f6ede7be
kostaskyritsis <kkyritsis@ws-70-81>
2023-11-03 13:52:40 +0200

    Fixed typo, pinned navbar and added navbar reduced opacity effect upon scroll down in index.qmd

---
+++ docs/navbar-effect.js
@@ -0,0 +1,9 @@
+// script.js
+window.addEventListener('scroll', function () {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 0) {
+        navbar.classList.add('transparent');
+    } else {
+        navbar.classList.remove('transparent');
+    }
+});
