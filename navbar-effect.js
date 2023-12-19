e21a53da0c91961b98a36c6f9eb0bfbbc71e5fe7
kostaskyritsis <konstantinoskyritsis@hotmail.com>
2023-12-19 16:27:01 +0200

    Updates on 4omics site

--- navbar-effect.js
+++ navbar-effect.js
@@ -1,9 +1,11 @@
-// script.js
+// navbar-effect during scroll
 window.addEventListener('scroll', function () {
     const navbar = document.querySelector('.navbar');
     if (window.scrollY > 0) {
+        // add class transparent
         navbar.classList.add('transparent');
     } else {
+        // remove class transparent
         navbar.classList.remove('transparent');
     }
 });
